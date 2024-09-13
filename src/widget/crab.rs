6e5f76373c84a14a2f19c6b149b58be17698ed5b
ckaznable <ckaznable@gmail.com>
2024-09-13 22:56:30 +0800

    feat: init ateshi kani ascii

--- src/widget/crab.rs
+++ src/widget/crab.rs
@@ -1,22 +1,44 @@
 use ratatui::{buffer::Buffer, layout::Rect, style::Color, widgets::Widget};
 
-type CrabAscii = [[char; 7]; 4];
+type CraBAscii = [[char; 9]; 5];
 /// fg, bg
-type CrabTheme = [[(Color, Color); 7]; 4];
+type CrabTheme = [[(Color, Color); 9]; 5];
 
-const CRAB: CrabAscii = [
-    ['▟', ' ', '●', ' ', '●', ' ', '▙'],
-    [' ', '▚', '▄', '▄', '▄', '▞', ' '],
-    [' ', '▐', '▒', '▓', '▒', '▌', ' '],
-    [' ', '▘', ' ', ' ', ' ', '▝', ' '],
+const CRAB: CraBAscii = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '▟', ' ', ' ', ' ', ' ', ' ', '▙', ' '],
+    [' ', ' ', '▐', '●', '█', '●', '▌', ' ', ' '],
+    ['/', '-', '▐', '█', '█', '█', '▌', '-', '\\'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+];
+
+const KANI: CraBAscii = [
+    [' ', ' ', '4', '4', '.', '5', ' ', ' ', ' '],
+    ['▟', ' ', ' ', '▂', '▁', '▂', ' ', ' ', '▙'],
+    [' ', '▗', '▄', '▄', '▄', '▄', '▄', '▖', ' '],
+    ['▞', ' ', '█', '▾', '█', '▾', '█', ' ', '▚'],
+    ['/', '-', '█', '▙', '▂', '▟', '█', '-', '\\'],
 ];
 
 use Color::*;
 const CRAB_THEME: CrabTheme = [
-    [(Red, Reset), (Reset, Reset), (Black, Reset), (Reset, Reset), (Black, Reset), (Reset, Reset), (Red, Reset)],
-    [(Reset, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Reset, Reset)],
-    [(Reset, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Reset, Reset)],
-    [(Reset, Reset), (Red, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Red, Reset), (Reset, Reset)],
+    [(Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset)],
+    [(Reset, Reset), (Red, Reset), (Reset, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Reset, Reset), (Red, Reset), (Reset, Reset)],
+    [(Reset, Reset), (Reset, Reset), (Red, Reset), (Black, Red), (Red, Reset), (Black, Red), (Red, Reset), (Reset, Reset), (Reset, Reset)],
+    [(Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset), (Red, Reset)],
+    [(Reset, Reset), (Reset, Reset), (Red, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Red, Reset), (Reset, Reset), (Reset, Reset)],
+];
+
+const AQUA_PINK: Color = Color::Rgb(235, 172, 219);
+const AQUA_BLUE: Color = Color::Rgb(48, 57, 113);
+const WHITE: Color = Color::Rgb(255, 255, 255);
+const AQUA: Color = Color::Rgb(176, 234, 253);
+const KANI_THEME: CrabTheme = [
+    [(Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset), (Reset, Reset)],
+    [(Red, Reset), (Red, Reset), (WHITE, Reset), (WHITE, Reset), (WHITE, Reset), (WHITE, Reset), (WHITE, Reset), (Red, Reset), (Red, Reset)],
+    [(Reset, Reset), (AQUA, Reset), (AQUA_PINK, WHITE), (AQUA_PINK, WHITE), (AQUA_PINK, WHITE), (AQUA_PINK, WHITE), (AQUA_PINK, WHITE), (AQUA, Reset), (Reset, Reset)],
+    [(AQUA_PINK, Reset), (Reset, Reset), (Red, Reset), (WHITE, Red), (Red, Reset), (WHITE, Red), (Red, Reset), (Reset, Reset), (AQUA_PINK, Reset)],
+    [(Red, Reset), (Red, Reset), (AQUA_BLUE, Reset), (AQUA_BLUE, WHITE), (AQUA_BLUE, WHITE), (AQUA_BLUE, WHITE), (AQUA_BLUE, Reset), (Red, Reset), (Red, Reset)],
 ];
 
 pub struct Crab;
@@ -42,7 +64,8 @@ impl Widget for Crab {
 
                 let pos = (left + x as u16, top + y as u16);
                 if let Some(cell) = buf.cell_mut(pos) {
-                    cell.set_char(*c).set_fg(CRAB_THEME[y][x].0);
+                    let theme = CRAB_THEME[y][x];
+                    cell.set_char(*c).set_fg(theme.0).set_bg(theme.1);
                 }
             })
         });
